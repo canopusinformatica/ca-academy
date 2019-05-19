23d4aff2f0b3c08b8334c6ecb95915df7d08bdea
Níkolas Vargas <vargasnikolass@gmail.com>
2019-05-19 19:15:44 -0300

    setup rest_framework

--- server/urls.py
+++ server/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from server.api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
