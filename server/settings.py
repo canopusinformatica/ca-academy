05fcad922a82f910f9ef663e622990c694c6343f
Níkolas Vargas <vargasnikolass@gmail.com>
2019-05-19 22:28:17 -0300

    cors configuration

--- server/settings.py
+++ server/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
+
+# setting file cors temporary develop configuration
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+)
 
 TEMPLATES = [
     {
